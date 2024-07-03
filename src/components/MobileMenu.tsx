d035040cb474d963d43456fb66dc341bfe895a14
sachin-source <sachinmgvt@gmail.com>
2024-07-03 19:27:52 +0530

    hiding the mobile menu for medium devices

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -6,7 +6,7 @@ import React, { useState } from 'react'
 const MobileMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <div className='md:hidden' >
         <div className="flex flex-col gap-[4.5px] cursor-pointer" onClick={() => setIsOpen((prev) => !prev)} >
             <div className={`w-6 h-1 bg-blue-500 rounded-sm ${ isOpen ? "rotate-45" : "" } origin-left ease-in-out duration-500`} />
             <div className={ `w-6 h-1 bg-blue-500 rounded-sm ${ isOpen ? "opacity-0" : "" } ease-in-out duration-500` } />
