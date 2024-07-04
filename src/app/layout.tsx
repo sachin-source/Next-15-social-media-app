20683a155d544e84bf7098362f025b39a2f4ad06
sachin-source <sachinmgvt@gmail.com>
2024-07-04 11:16:47 +0530

    clerk auth in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <div className="w-full px-4 bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
@@ -26,5 +28,6 @@ export default function RootLayout({
         </div>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
 import Image from 'next/image'
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -30,6 +31,13 @@ const Navbar = () => {
       </div>
       {/* Right */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"></div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>Signed In</SignedIn>
+          <SignedOut>Signed Out</SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
