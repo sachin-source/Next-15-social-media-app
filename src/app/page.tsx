713fd617acff32698ae78bfa8a558d982b09c236
sachin-source <sachinmgvt@gmail.com>
2024-07-05 10:59:15 +0530

    leftmenu init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import LeftMenu from "@/components/LeftMenu"
+
 const Homepage = () => {
   return (
     <div className='flex gap-6 xl:block'>
       {/* Left */}
-      <div className="hidden xl:block w-[20%]"></div>
+      <div className="hidden xl:block w-[20%]"> <LeftMenu/> </div>
       {/* Center */}
       <div className="w-full lg:w-[70%] xl:w-[50%]"></div>
       {/* Right */}
