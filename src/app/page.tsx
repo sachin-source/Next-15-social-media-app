678baf5ebe3c0141010c6c6f478df34cced1aceb
sachin-source <sachinmgvt@gmail.com>
2024-07-06 10:54:33 +0530

    UI layout issue fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Homepage = () => {
       <div className="hidden xl:block w-[20%]"> <LeftMenu/> </div>
       {/* Center */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
-        <div className="">
+        <div className="flex flex-col gap-6">
           <Stories/>
           <AddPost/>
           <Feed/>
