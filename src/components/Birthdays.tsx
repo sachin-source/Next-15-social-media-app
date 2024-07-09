93e1acb2de1f920dd5425c011192113674528b1a
sachin-source <sachinmgvt@gmail.com>
2024-07-09 19:31:12 +0530

    birthday section complete

--- src/components/Birthdays.tsx
+++ src/components/Birthdays.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Birthdays = () => {
   return (
-    <div className='p-4 bg-white rounded-lg shadow-md text-sm' >
+    <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4' >
       <div className="flex justify-between items-center font-medium">
         <span className='text-gray-500'>Birthdays</span>
       </div>
@@ -14,9 +15,16 @@ const Birthdays = () => {
           <span>Wayne Burton</span>
         </div>
         <div className="flex gap-3 justify-end">
-          <Image src="/accept.png" alt='' width={20} height={20} className='cursor-pointer' />
-          <Image src="/reject.png" alt='' width={20} height={20} className='cursor-pointer' />
+          <button className='bg-blue-500 text-white text-xs px-2 py-1 rounded-md' >Celebrate</button>
         </div>
+      </div>
+      {/* UPCOMING */}
+      <div className="p-4 bg-slate-100 rounded-lg flex items-center gap-4">
+        <Image src="/gift.png" alt='' width={24} height={24}  />
+        <Link href='/' className='flex flex-col gap-1 text-xs' >
+        <span className="text-gray-700 font-semibold">Upcoming Birthdays</span>
+        <span className="text-gray-500">See other 16 have upcoming birthdays</span>
+        </Link>
       </div>
     </div>
   )
