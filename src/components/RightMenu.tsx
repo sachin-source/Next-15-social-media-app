c99a94d623105443e0645c0227a8f5c244cdebe2
sachin-source <sachinmgvt@gmail.com>
2024-07-09 10:03:18 +0530

    userId set to not requirred

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -3,7 +3,7 @@ import FriendRequests from './FriendRequests'
 import Birthdays from './Birthdays'
 import Ad from './Ad'
 
-const RightMenu = ({ userId } : {userId: string}) => {
+const RightMenu = ({ userId } : {userId?: string}) => {
   return (
     <div className='flex flex-col gap-6'>
       <FriendRequests />
