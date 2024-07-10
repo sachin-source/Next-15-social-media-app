3c5d99ebfb66adf8c149335c3b6f64a087592e9d
sachin-source <sachinmgvt@gmail.com>
2024-07-10 18:49:43 +0530

    started with profile page UI

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Feed from '@/components/Feed'
+import LeftMenu from '@/components/LeftMenu'
+import RightMenu from '@/components/RightMenu'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex gap-6 pt-6 xl:flex'>
+      {/* Left */}
+      <div className="hidden xl:block w-[20%]"> <LeftMenu /> </div>
+      {/* Center */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Feed />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="hidden lg:block w-[30%]"> <RightMenu userId='test' /> </div>
+    </div>
   )
 }
 
