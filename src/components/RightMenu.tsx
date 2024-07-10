fb472c4b2b9bd7f3eb2aa0e27eafd79bf1e894dd
sachin-source <sachinmgvt@gmail.com>
2024-07-10 18:50:19 +0530

    customizing right menu for profile page

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import FriendRequests from './FriendRequests'
 import Birthdays from './Birthdays'
 import Ad from './Ad'
+import UserInfoCard from './UserInfoCard'
+import UserMediaCard from './UserMediaCard'
 
 const RightMenu = ({ userId } : {userId?: string}) => {
   return (
     <div className='flex flex-col gap-6'>
+      {userId ? (
+        <>
+        <UserInfoCard userId={userId} />
+        <UserMediaCard userId={userId} />
+        </>
+      ) : null }
       <FriendRequests />
       <Birthdays />
       <Ad size='md' />
