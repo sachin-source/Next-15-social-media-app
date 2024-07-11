c53f2e75c4890724d08462bdbe817aafc9316988
sachin-source <sachinmgvt@gmail.com>
2024-07-11 19:21:21 +0530

    left menu in-progress

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import ProfileCard from './ProfileCard'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const LeftMenu = () => {
+const LeftMenu = ({type} : {type:"home" | "profile"}) => {
   return (
-    <div>LeftMenu</div>
+    <div className='flex flex-col gap-6' >
+      { type === 'home' && <ProfileCard/> }
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+        <Link href="/" >
+        <Image src="/posts.png" alt='' width={20} height={20} />
+        <span>My Posts</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
