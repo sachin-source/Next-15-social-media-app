0f3bfd6dddb7d45ff8af31b21d553945707d0d15
sachin-source <sachinmgvt@gmail.com>
2024-07-12 13:34:40 +0530

    leftbar customization for profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -6,7 +6,7 @@ const ProfilePage = () => {
   return (
     <div className='flex gap-6 pt-6 xl:flex'>
       {/* Left */}
-      <div className="hidden xl:block w-[20%]"> <LeftMenu /> </div>
+      <div className="hidden xl:block w-[20%]"> <LeftMenu type='profile' /> </div>
       {/* Center */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
