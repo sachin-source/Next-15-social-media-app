c4b53b8ac525639f47497bfa71405e22a33998fd
sachin-source <sachinmgvt@gmail.com>
2024-07-15 20:00:48 +0530

    profileCard server component in-progress

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,15 +1,34 @@
+import prisma from '@/lib/client';
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
-import React from 'react'
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+  const { userId } = auth();
+
+  if(!userId) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId
+    },
+    include: {
+      _count: {
+        select:{
+          followers: true
+        }
+      }
+    }
+  });
+  console.log(user);
+  if(!user) return null;
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6'>
       <div className="h-20 relative">
-          <Image src='https://images.pexels.com/photos/25473496/pexels-photo-25473496/free-photo-of-a-dark-stairway-with-a-light-shining-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' fill className='rounded-md object-cover' />
-        <Image width={48} height={48} src='https://images.pexels.com/photos/17881368/pexels-photo-17881368/free-photo-of-young-woman-in-a-t-shirt-and-jeans.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' className='rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10' />
+          <Image src={user?.cover || '/noCover.png'} alt='' fill className='rounded-md object-cover' />
+        <Image src={user?.avatar || '/noAvatar.png'} width={48} height={48} alt='' className='rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10' />
       </div>
       <div className="h-20 flex flex-col gap-2 items-center">
-        <span className="font-semibold">Edward Gabriel May</span>
+        <span className="font-semibold"> { (user.name && user.surname) ? user.name + " " + user.surname : user.username } </span>
         <div className="flex items-center gap-4">
         <div className="flex">
         <Image width={12} height={12} src='https://images.pexels.com/photos/17881368/pexels-photo-17881368/free-photo-of-young-woman-in-a-t-shirt-and-jeans.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' className='rounded-full object-cover w-3 h-3' />
