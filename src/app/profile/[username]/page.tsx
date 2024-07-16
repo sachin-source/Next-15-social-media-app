29be81089f0c20a65cff91f5f3531d5b5d29d667
sachin-source <sachinmgvt@gmail.com>
2024-07-16 19:58:08 +0530

    user profile page in progress

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,9 +1,44 @@
 import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
+import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
-const ProfilePage = () => {
+const ProfilePage = async ({params}:{params:{username:string}}) => {
+  const username = params.username;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          followings: true,
+          posts: true
+        }
+      }
+    }
+  })
+
+  if(!user) return notFound();
+
+  const { userId: currentUserId } = auth();
+
+  let isBlocked;
+
+  if(currentUserId){
+    await prisma.block.findFirst({
+      where: {
+        blockerId: user.id,
+        blockedId: currentUserId,
+      }
+    })
+  }
+
   return (
     <div className='flex gap-6 pt-6 xl:flex'>
       {/* Left */}
