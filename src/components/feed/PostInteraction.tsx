3bc9b1a7a5850ee10e840b54e79678115d8be7d9
sachin-source <sachinmgvt@gmail.com>
2024-07-21 15:43:48 +0530

    like functionality with server action complete

--- src/components/feed/PostInteraction.tsx
+++ src/components/feed/PostInteraction.tsx
@@ -1,11 +1,12 @@
 "use client"
 
+import { switchLike } from "@/lib/actions";
 import { useAuth } from "@clerk/nextjs"
 import Image from "next/image"
 import { useOptimistic, useState } from "react";
 
-const PostInteraction = ({postId, likes, commentNumber}: {postId:number, likes:string[], commentNumber: number}) => {
-    const {isLoaded, userId} = useAuth();
+const PostInteraction = ({ postId, likes, commentNumber }: { postId: number, likes: string[], commentNumber: number }) => {
+    const { userId } = useAuth();
     const [likeState, setLikeState] = useState({
         likeCount: likes.length,
         isLiked: userId ? likes.includes(userId) : false
@@ -14,28 +15,32 @@ const PostInteraction = ({postId, likes, commentNumber}: {postId:number, likes:s
     const [optimisticLike, switchOptimisticLike] = useOptimistic(
         likeState,
         (state, value) => {
-        return {
-            likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
-            isLiked: !state.isLiked
-        }
-    })
+            return {
+                likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+                isLiked: !state.isLiked
+            }
+        })
 
     const likeAction = async () => {
         switchOptimisticLike("");
         try {
-            
+            switchLike(postId);
+            setLikeState(state => ({
+                likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+                isLiked: !state.isLiked
+            }))
         } catch (error) {
-            
+
         }
     }
-  return (
-    <div className="flex items-center justify-between text-sm my-4">
+    return (
+        <div className="flex items-center justify-between text-sm my-4">
             <div className="flex gap-8">
                 <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
                     <form action={likeAction}>
                         <button>
-                    <Image src={optimisticLike.isLiked ? "/liked.png" : "/like.png"} width={16} height={16} alt='' className='cursor-pointer' />
-                    </button>
+                            <Image src={optimisticLike.isLiked ? "/liked.png" : "/like.png"} width={16} height={16} alt='' className='cursor-pointer' />
+                        </button>
                     </form>
                     <span className='text-gray-300' >|</span>
                     <span className='text-gray-500' >{optimisticLike.likeCount} <span className='hidden md:inline' >Likes</span> </span>
@@ -47,14 +52,14 @@ const PostInteraction = ({postId, likes, commentNumber}: {postId:number, likes:s
                 </div>
             </div>
             <div className="">
-            <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
                     <Image src="/share.png" width={16} height={16} alt='' className='cursor-pointer' />
                     <span className='text-gray-300' >|</span>
                     <span className='text-gray-500' > <span className='hidden md:inline' >Shares</span> </span>
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default PostInteraction
