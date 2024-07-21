aabcd8febf3519acad177f478e5650a45e32ba7b
sachin-source <sachinmgvt@gmail.com>
2024-07-21 15:36:57 +0530

    like functionality integration

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import Comments from './Comments'
 import { Post as PostType, User } from '@prisma/client'
+import PostInteraction from './PostInteraction'
 
 type FeedPostType = PostType & {user:User} & {likes:[{userId:string}]} & {_count:{comments:number}}
 
@@ -26,27 +27,7 @@ const Post = ({post}:{post: FeedPostType}) => {
             <p>{post.desc}</p>
         </div>
         {/* INTERACTION */}
-        <div className="flex items-center justify-between text-sm my-4">
-            <div className="flex gap-8">
-                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                    <Image src="/like.png" width={16} height={16} alt='' className='cursor-pointer' />
-                    <span className='text-gray-300' >|</span>
-                    <span className='text-gray-500' >123 <span className='hidden md:inline' >Likes</span> </span>
-                </div>
-                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                    <Image src="/comment.png" width={16} height={16} alt='' className='cursor-pointer' />
-                    <span className='text-gray-300' >|</span>
-                    <span className='text-gray-500' >123 <span className='hidden md:inline' >Comments</span> </span>
-                </div>
-            </div>
-            <div className="">
-            <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                    <Image src="/share.png" width={16} height={16} alt='' className='cursor-pointer' />
-                    <span className='text-gray-300' >|</span>
-                    <span className='text-gray-500' > <span className='hidden md:inline' >Shares</span> </span>
-                </div>
-            </div>
-        </div>
+        <PostInteraction postId={post.id} likes={post.likes.map(like => like.userId)} commentNumber={post._count.comments} />
         <Comments />
     </div>
   )
