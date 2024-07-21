e3c96542f8e302bd41ccb7d35d36064f5ed3c86e
sachin-source <sachinmgvt@gmail.com>
2024-07-21 14:45:44 +0530

    update user with formstatus hook

--- src/components/rightMenu/UpdateButton.tsx
+++ src/components/rightMenu/UpdateButton.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+"use client"
+import { useFormStatus } from 'react-dom'
 
 const UpdateButton = () => {
+    const { pending } = useFormStatus();
   return (
-    <div>UpdateButton</div>
+    <button className="bg-blue-500 p-2 mt-2 rounded-md text-white disabled:bg-opacity-50 disabled:cursor-not-allowed" disabled={pending} >
+        {pending ? "Updating..." : "Update"}
+    </button>
   )
 }
 
