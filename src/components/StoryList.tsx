45065ca021bdb161bc3da870da3fb78ab71de1dd
sachin-source <sachinmgvt@gmail.com>
2024-07-24 11:27:51 +0530

    add story in progress

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -2,6 +2,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import { Story, User } from "@prisma/client";
+import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useOptimistic, useState } from "react";
 
@@ -15,10 +16,24 @@ const StoryList = ({ stories}: { stories: StoryWithUser[] }) => {
     const [ optimisticStories, addOptimisticStory ] = useOptimistic(StoryList, (state:any, value:StoryWithUser) => [value, ...state]);
     return (
         <div className="">
-            <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/7615526/pexels-photo-7615526.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={80} height={80} className='w-20 h-20 rounded-full ring-2' />
-                <span className="font-medium">Sachin</span>
+            <CldUploadWidget uploadPreset="social" onSuccess={(result, {widget}) => { setImg(result.info); widget.close() }}>
+            {({ open }) => {
+              return (
+                <div onClick={() => open()} className="flex flex-col items-center gap-2 cursor-pointer relative">
+                <Image src={user?.imageUrl || "noAvatar.png"} alt='' width={80} height={80} className='w-20 h-20 rounded-full ring-2' />
+                <span className="font-medium">Add a Story</span>
+                <div className="absolute text-6xl text-gray-200 top-1" >+</div>
             </div>
+              )
+            }}
+          </CldUploadWidget>
+            {/* STORIES */}
+            {optimisticStories.map((story) => (
+                <div key={story.id} className="flex flex-col items-center gap-2 cursor-pointer">
+                <Image src={story.user.avatar || "noAvatar.png"} alt='' width={80} height={80} className='w-20 h-20 rounded-full ring-2' />
+                <span className="font-medium">{ story.user.name || story.user.username }</span>
+            </div>
+            ))}
         </div>
     )
 }
