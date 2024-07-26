780628750290d8b5e1c084c998a90c1ab4f88a43
sachin-source <sachinmgvt@gmail.com>
2024-07-26 19:49:19 +0530

    profile link add

--- src/components/leftMenu/ProfileCard.tsx
+++ src/components/leftMenu/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/lib/client';
 import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import Link from 'next/link';
 
 const ProfileCard = async () => {
   const { userId } = auth();
@@ -37,7 +38,9 @@ const ProfileCard = async () => {
         </div>
         <span className='text-xs text-gray-500' >{user._count.followers} Followers</span>
         </div>
+        <Link href={"/profile/" + user.username } >
         <button className='bg-blue-500 text-white text-xs p-2 rounded-md' >My Profile</button>
+        </Link>
       </div>
     </div>
   )
